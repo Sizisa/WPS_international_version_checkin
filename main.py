a322fca3d2ac143c4aea8c90e38df27a37052015
Sizisa <1327438873@qq.com>
2020-07-12 18:44:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import checkin
 import os
 import requests
   
-def msgFormat(username,num,flag):
-    str='----\n'\
-        +'* 用户'+'1'+'\n'\
+def msgFormat(i,username,num,flag):
+    string='----\n'\
+        +'* 用户'+str(i)+'\n'\
         +'\t> `用户名：'+username+'`\n'\
         +'\t\n'\
         +'\t>`已签到 '+str(num)+' 天`\n'\
         +'\t\n'\
         +'\t>`今天已签到：'+flag+'`\n'\
-        +'\t\n'+''
-    return str
+        +'\t\n'
+    return string
 
 def send(sckey,title,msg):
     url='http://sc.ftqq.com/%s.send'%(sckey)
@@ -28,9 +28,9 @@ def wpsCheckin(usernames,password,sckey):
         print('===========为第'+str(i+1)+'个用户签到，用户名：'+username+'============')
         (flag,num)=checkin.checkin(username,password,sckey)
         if flag==1:
-            string+=msgFormat(username,num,'是')
+            string+=msgFormat(i+1,username,num,'是')
         elif flag==0:
-            string+=msgFormat(username,num,'否')
+            string+=msgFormat(i+1,username,num,'否')
         print('========================================')
     send(sckey,'WPS国际版签到通知',string)
 
@@ -38,4 +38,5 @@ if __name__ == "__main__":
     username = os.environ['username']
     password = os.environ['password']
     sckey = os.environ['sckey']
+    
     wpsCheckin(username,password,sckey)
